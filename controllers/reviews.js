7ebb3c636e310b839f7c428794c73b58f364f793
Neeraj Singh <neeraj123466789neeraj@gmail.com>
2025-09-13 18:36:24 +0530

    Add Project Files

---
+++ controllers/reviews.js
@@ -0,0 +1,31 @@
+const Listing = require("../models/listing.js");
+const Review = require("../models/review.js");
+
+module.exports.createReview = async(req,res)=>{
+    
+     let listing = await Listing.findById(req.params.id);
+     let newReview = new Review(req.body.review);
+        newReview.author = req.user._id;
+        console.log(newReview);
+     listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+    req.flash("success","New Review Created Successfully!");
+     //console.log(newReview);
+     //console.log(listing);
+     //console.log(listing.reviews);
+     //console.log(listing.reviews[0]);
+     //console.log(listing.reviews[0]._id);
+     //console.log(newReview._id);
+     //console.log(listing.reviews[0]._id.equals(newReview._id));
+    res.redirect(`/listings/${listing._id}`);
+};
+
+module.exports.destroyReview = async(req,res)=>{
+    let {id, reviewId} = req.params;
+    await Listing.findByIdAndUpdate(id,{$pull : {reviews : reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    req.flash("success","Review Deleted Successfully!");
+
+    res.redirect(`/listings/${id}`);
+};
